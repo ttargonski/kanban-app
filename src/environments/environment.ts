dbbbac64be98201a47e1c8e66e5ec913504338ab
ttargonski <tomasz.targonski95@gmail.com>
2023-09-11 13:33:45 +0200

    gh deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,3 @@
 export const environment = {
-  firebase: {
-    apiKey: 'AIzaSyC1o4pqx7eOTrtFqYKM7uFqyKNlT8BjIj0',
-    authDomain: 'angular-login-b9d7d.firebaseapp.com',
-    projectId: 'angular-login-b9d7d',
-    storageBucket: 'angular-login-b9d7d.appspot.com',
-    messagingSenderId: '807894414422',
-    appId: '1:807894414422:web:998ff3f279bee39171b7f7',
-  },
+  firebase: {},
 };
